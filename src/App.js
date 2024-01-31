670090fab833751eb696ef507656c446c634abc1
Paul Johann Flade <paul-johann@flade-privat.de>
2024-01-31 12:18:48 +0100

    challage solved

--- src/App.js
+++ src/App.js
@@ -2,5 +2,21 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
+}
+
+function Article() {
+  return (
+    <article>
+      <h2 className="article__title">Article with Attributes</h2>
+      <label htmlFor="name">Name</label>
+      <input id="name"></input>
+      <a
+        href="https://www.youtube.com/watch?v=qJqjcxLvEwg&t=1512s"
+        className="article__link"
+      >
+        click
+      </a>
+    </article>
+  );
 }
